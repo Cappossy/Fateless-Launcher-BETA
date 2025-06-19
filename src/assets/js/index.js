78a222b60349f1dfb9b04b1616131663364f1b96
Cappossy <117684593+Cappossy@users.noreply.github.com>
2025-06-19 22:56:58 +0200

    release 1.0.0 Fix ?
    Fix ?

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "", "author": "" },
-            { "message": "", "author": "" },
-            { "message": "", "author": "" }
+            { "message": "", "": "" },
+            { "message": "", "": "" },
+            { "message": "", "": "" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
