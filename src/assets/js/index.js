48fc1db28def3b729f99ea3e0ccffa60a5e09a0c
Cappossy <117684593+Cappossy@users.noreply.github.com>
2025-06-29 16:40:13 +0200

    Release 1.0.2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "", author "Cappossy" },
-            { "message": "", author "Cappossy" },
-            { "message": "", author "Cappossy" }
+            { "message": "", "author" "Cappossy" },
+            { "message": "", "author" "Cappossy" },
+            { "message": "", "author" "Cappossy" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
